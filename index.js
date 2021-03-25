9a5aa65643d0248c89d696d5351d4715b889d3a5
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2021-03-24 22:09:11 -0500

    remove test picture and stuff

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const progressive = require('is-progressive-jpeg');
 var read = require('fs-readdir-recursive');
 const fs = require('fs');
 let args = process.argv.slice(2);
-const dir = args.length == 0 ? './' : args[0]
+const dir = args.length == 0 ? '.' : args[0]
 
 read(dir).filter(function (file) {
     return file.substr(-4) === '.jpg';
 }).forEach(async (e) => {
     let img = e.replace(`\\`, '/')
-    let imgpath = `${dir}${img}`
+    let imgpath = `${dir}/${img}`
     let image = progressive.setImage(imgpath)
     let isP = await image.isProgressive()
+    console.log(`Converted ${imgpath}`)
     if (isP) {
         const data = await sharp(imgpath)
             .jpeg({ quality: 100, progressive: false })
